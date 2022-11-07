2480d4a9b7fe1df73c5a05113bfaeb3824b9a2e9
Matthew Tiberio <matthewtiberio@Matthews-MacBook-Air.local>
2022-11-07 13:54:51 -0700

    Add Scotch model, admin user, detail template

--- scotchcollector/main_app/urls.py
+++ scotchcollector/main_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('scotch/', views.scotch_index, name='index'),
+    path('scotch/<int:scotch_id>/', views.scotch_detail, name='detail'),
 ]
--- scotchcollector/main_app/views.py
+++ scotchcollector/main_app/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Scotch
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Hello Scotch Lovers</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
+def scotch_index(request):
+    scotchs = Scotch.objects.all()
+    return render(request, 'scotch/index.html', { 'scotchs': scotchs })
+
+def scotch_detail(request, scotch_id):
+    scotch = Scotch.objects.get(id=scotch_id)
+    return render(request, 'scotch/detail.html', { 'scotch': scotch })
