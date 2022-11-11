70f40078bff38901943c8270cf7c2af91bec3518
Matthew Tiberio <matthewtiberio@Matthews-MacBook-Air.local>
2022-11-10 17:53:20 -0700

    Update Details page styling

--- whiskeycollector/main_app/urls.py
+++ whiskeycollector/main_app/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('whiskey/', views.whiskey_index, name='index'),
     path('whiskey/<int:whiskey_id>/', views.whiskey_detail, name='detail'),
-    # path('whiskey/create/', views.WhiskeyCreate.as_view(), name='whiskey_create'),
     path('whiskey/create/', views.whiskey_create, name="whiskey_create"),
     path('whiskey/add/', views.whiskey_add, name="whiskey_add"),
     path('whiskey/<int:pk>/update/', views.WhiskeyUpdate.as_view(), name='whiskey_update'),
